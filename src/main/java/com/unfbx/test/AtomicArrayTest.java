642eef69c5e8a6541364e8e96a9af1b67cfb89b7
guorutao <guorutao@sundear.com>
2021-08-18 18:28:53 +0800

    3

--- src/main/java/com/unfbx/test/AtomicArrayTest.java
+++ src/main/java/com/unfbx/test/AtomicArrayTest.java
@@ -31,6 +31,17 @@ public class AtomicArrayTest {
         );
     }
 
+    /**
+     * // supplier 提供者 无中生有 ()->结果
+     * // function 函数 一个参数一个结果 (参数)->结果 , BiFunction (参数1,参数2)->结果
+     * // consumer 消费者 一个参数没结果 (参数)->void, BiConsumer (参数1,参数2)->
+     *
+     * @param arraySupplier 参数1，提供数组、可以是线程不安全数组或线程安全数组
+     * @param arrayFunction 参数2，获取数组长度的方法
+     * @param putConsumer   参数3，自增方法，回传 array, index
+     * @param printConsumer 参数4，打印数组的方法
+     * @param <T>
+     */
     public static <T> void demo(
             Supplier<T> arraySupplier,
             Function<T, Integer> arrayFunction,
